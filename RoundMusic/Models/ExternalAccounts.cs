b02bc7c664a86a56f6b0928653d4b61aa93dd488
Jesus Mendez <J.mendez@caribemedia.com.do>
2016-11-09 21:13:13 -0400

    +Agregado font awesome +Agregado el connectionString para azure +Mejorado la muestra de resultados.

---
+++ RoundMusic/Models/ExternalAccounts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace RoundMusic.Models
+{
+    public class ExternalAccounts
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Key]
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string TokenId { get; set; }
+        public string ServiceName { get; set; }
+
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+    }
+}
--- RoundMusic/Models/IdentityModels.cs
+++ RoundMusic/Models/IdentityModels.cs
@@ -20,6 +20,7 @@ namespace RoundMusic.Models
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
+        public DbSet<ExternalAccounts> ExternalAccounts { get; set; }
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
